8f7551f3fa237156f259b0d18fcb6080f386ba74
Justin Chau <justin.jay.chau@gmail.com>
2022-07-22 17:07:12 -0400

    Visualize path.

--- localization-visualizer/src/App.tsx
+++ localization-visualizer/src/App.tsx
@@ -1,34 +1,81 @@
-import { useControls } from "leva";
+import { useControls, button } from "leva";
 import useWebsocket from "react-use-websocket";
 import { Canvas } from "@react-three/fiber";
-import { useEffect } from "react";
-import { OrbitControls } from "@react-three/drei";
+import { useEffect, useMemo } from "react";
+import { Line, MapControls } from "@react-three/drei";
+import { AxesHelper } from "three";
 
 function App() {
-  const [{ x, y, z }, set] = useControls(() => ({ x: 0, y: 0, z: 0 }));
-
   const { lastMessage } = useWebsocket("ws://localhost:5555");
+  const { sendMessage } = useWebsocket("ws://localhost:5555/capture");
+
+  const [{ x, y, z }, set] = useControls(() => ({
+    x: 0,
+    y: 0,
+    z: 0,
+    "Capture Image": button(() => {
+      sendMessage("capture");
+    }),
+  }));
+
+  const parsed = useMemo(() => {
+    if (lastMessage?.data) {
+      return JSON.parse(lastMessage.data);
+    }
+
+    return undefined;
+  }, [lastMessage]);
 
   useEffect(() => {
-    const position = lastMessage?.data
-      ? JSON.parse(lastMessage.data)
-      : [0, 0, 0];
-    set({ x: -position[0] * 10, y: -position[1] * 10, z: -position[2] * 10 });
-  }, [lastMessage?.data]);
+    if (parsed) {
+      set({ x: parsed.robot_pose.x, y: parsed.robot_pose.y, z: 0 });
+    }
+  }, [parsed, set]);
 
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <Canvas>
-        <OrbitControls />
-        <mesh position={[0, 0, 0]}>
-          <sphereGeometry />
-          <meshBasicMaterial />
-        </mesh>
+    <div style={{ width: "100vw", height: "100vh", padding: 0, margin: 0 }}>
+      <Canvas
+        orthographic
+        camera={{ position: [0, 0, 50], zoom: 15, up: [0, 0, 1], far: 10000 }}
+      >
+        <ambientLight />
+        <primitive object={new AxesHelper(5)} />
+
+        {parsed &&
+          parsed.obstacle_list.map((obstacle: any) => (
+            <mesh position={[obstacle.pose.x, obstacle.pose.y, 0]}>
+              <sphereGeometry args={[obstacle.radius, 32, 16]} />
+              <meshStandardMaterial color="hotpink" />
+            </mesh>
+          ))}
+
+        {parsed &&
+          parsed.path.map((pose: any, index: number) => {
+            if (index < parsed.path.length - 1) {
+              return (
+                <Line
+                  points={[
+                    [pose.x, pose.y, 0],
+                    [parsed.path[index + 1].x, parsed.path[index + 1].y, 0],
+                  ]}
+                />
+              );
+            }
+          })}
+
+        {parsed && (
+          <mesh position={[parsed.goal_pose.x, parsed.goal_pose.y, 0]}>
+            <sphereGeometry args={[0.2, 32, 16]} />
+            <meshStandardMaterial color="red" />
+          </mesh>
+        )}
 
         <mesh position={[x, y, z]}>
-          <boxGeometry />
-          <meshStandardMaterial />
+          <sphereGeometry args={[0.5, 32, 16]} />
+          <meshStandardMaterial color="green" />
         </mesh>
+
+        <MapControls />
       </Canvas>
     </div>
   );
